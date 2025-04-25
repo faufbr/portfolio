2e0b3e3dc0bf85d4f9dda0beabaed81c0d405b90
faufbr <faustine.fabre44@gmail.com>
2025-04-25 09:24:36 +0200

    modifs + suppr blog

--- src/consts.ts
+++ src/consts.ts
@@ -1,44 +1,35 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Astro Nano",
-  EMAIL: "markhorn.dev@gmail.com",
-  NUM_POSTS_ON_HOMEPAGE: 3,
+  NAME: "Portfolio",
+  EMAIL: "faustine.fabre44@gmail.com",
+  NUM_POSTS_ON_HOMEPAGE: 0,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
-  TITLE: "Home",
-  DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
-};
-
-export const BLOG: Metadata = {
-  TITLE: "Blog",
-  DESCRIPTION: "A collection of articles on topics I am passionate about.",
+  TITLE: "Accueil",
+  DESCRIPTION: "Mon portfolio où je partage mon parcours scolaire et mon expérience professionnelle.",
 };
 
 export const WORK: Metadata = {
-  TITLE: "Work",
-  DESCRIPTION: "Where I have worked and what I have done.",
+  TITLE: "Expérience professionnelle",
+  DESCRIPTION: "Mon expérience professionnelle.",
 };
 
 export const PROJECTS: Metadata = {
-  TITLE: "Projects",
-  DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
+  TITLE: "Projets",
+  DESCRIPTION: "Mes projets.",
 };
 
 export const SOCIALS: Socials = [
   { 
-    NAME: "twitter-x",
-    HREF: "https://twitter.com/markhorn_dev",
-  },
-  { 
     NAME: "github",
-    HREF: "https://github.com/markhorn-dev"
+    HREF: "https://github.com/faufbr"
   },
   { 
     NAME: "linkedin",
-    HREF: "https://www.linkedin.com/in/markhorn-dev",
+    HREF: "https://www.linkedin.com/in/faustine-fabre-960a48210/",
   }
 ];
--- src/content/work/buttonhop.md
+++ src/content/work/buttonhop.md
@@ -1,8 +1,8 @@
 ---
-company: "Google"
-role: "Staff Software Engineer"
-dateStart: "11/27/2022"
-dateEnd: "Current"
+company: "ButtonHop"
+role: "Stagiaire développeuse"
+dateStart: "01/06/2025"
+dateEnd: "02/14/2025"
 ---
 
 Sit amet consectetur adipisicing elit. Iure illo neque tempora, voluptatem est quaerat voluptas praesentium ipsa dolorem dignissimos nulla ratione distinctio quae maiores eligendi nostrum? Quibusdam, debitis voluptatum, lorem ipsum dolor.
